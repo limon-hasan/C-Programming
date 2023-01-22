3e1a30f45bb7d295e28a42c28b9ee52a0c9b599a
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-22 19:38:53 +0600

    Add files via upload

---
+++ C C++/test.c
@@ -0,0 +1,15 @@
+//Write a program that prints an integer number
+
+#include<stdio.h>
+int main()
+{
+    float num1 = 10.2;
+    double num2 = 50.234555555;
+    char ch = 'a';
+
+    printf("num1 = %f\n",num1);
+    printf("num2 = %lf\n",num2);
+    printf("ch = %c",ch);
+
+    return 0;
+}
