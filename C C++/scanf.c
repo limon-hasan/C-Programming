cb3e00abae29a68951cd807c8c7a74ba7d74ea50
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-23 12:35:50 +0600

    Update scanf.c

--- C C++/scanf.c
+++ C C++/scanf.c
@@ -14,3 +14,24 @@ printf("Numbers are : %d %d\n",num1,num2);
 
     return 0;
 }
+
+
+অথবা-----
+
+    
+#include<stdio.h>
+int main()
+{
+int num1,num2;
+
+printf("Please enter two integers :");
+scanf("%d",&num1);
+scanf("%d",&num2);
+
+
+printf("Numbers are : %d, %d\n",num1,num2);
+
+
+    return 0;
+}
+    
