9e3ca9cd420a668b51882b85a7302c742b5f2989
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-23 12:14:42 +0600

    Add files via upload

---
+++ C C++/scanf.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int num1,num2;
+
+printf("Please enter First integer :");
+scanf("%d",&num1);
+printf("Please enter Second integer :");
+scanf("%d",&num2);
+
+
+printf("Numbers are : %d %d\n",num1,num2);
+
+
+    return 0;
+}
