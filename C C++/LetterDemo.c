7a9cd0f6b280dbc24ef4f68d706bb807da3088e2
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-24 01:32:23 +0600

    Update and rename LetterDemo1.c to LetterDemo.c

---
+++ C C++/LetterDemo.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    char lower;
+    printf("Enter any lowercase letter : ");
+    scanf("%c",&lower);  //a=97
+
+    printf("The Uppercase letter is = %c",lower-32);  //A=65
+
+    getch();
+}
+
+_________________________________________________________________________________________________________________________________________________________________________-___
+
+#include<stdio.h>
+int main()
+{
+    char h;
+    printf("Enter any Uppercase letter : ");
+    scanf("%c",&h);
+
+    printf("The lowercase letter is = %c",h+32);
+    getch();
+}
