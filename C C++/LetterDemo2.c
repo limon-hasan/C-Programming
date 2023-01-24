6db09bcec99433165d3ce4f08c7420472d09a8e3
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-24 11:52:09 +0600

    Update LetterDemo2.c

--- C C++/LetterDemo2.c
+++ C C++/LetterDemo2.c
@@ -10,3 +10,18 @@ int main()
 
     return 0;
 }
+
+__________________________________________________________________________________________________________________________________________________________________________________________________
+    
+#include<stdio.h>
+int main()
+{
+    char a,b;
+    printf("Enter an uppercase letter : ");
+    scanf("%c",&a);
+
+    b = tolower(a);
+    printf("The lowercase letter = %c",b);
+    
+    return 0;
+}    
