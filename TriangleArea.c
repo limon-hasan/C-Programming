bf6c5d00de78464683af3ff98fc9a18ca9fff876
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-25 14:38:58 +0600

    Update TriangleArea.c

--- TriangleArea.c
+++ TriangleArea.c
@@ -15,3 +15,26 @@ int main()
 
     return 0;
 }
+
+
+OR----------------
+    
+    
+//Write a program that takes that calculates the area of triangles
+#include<stdio.h>
+int main()
+{
+    int base,height;
+    float area;
+    printf("Base is = ");
+    scanf("%d",&base);
+
+    printf("Height is = ");
+    scanf("%d",&height);
+
+    area = 1/(float)2 * base * height;
+    printf("Area of triangle is = %.1f\n",area);
+
+
+    return 0;
+}
