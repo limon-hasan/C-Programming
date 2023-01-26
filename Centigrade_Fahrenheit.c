bb95d772f67aba8627885571761b6be5f47d54e2
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-26 23:17:52 +0600

    Add files via upload

---
+++ Centigrade_Fahrenheit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float C,F;
+    printf("Enter centigrade = ");
+    scanf("%f",&C);
+
+    F = (C*1.8)+32;
+    printf("Farenheit is = %.1f",F);
+
+
+    return 0;
+}
