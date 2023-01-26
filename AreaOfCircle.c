7d9f3cb72a235288c5caeb3fdd7946739aac7bcd
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-26 22:28:45 +0600

    Add files via upload

---
+++ AreaOfCircle.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int r;
+    double area;
+    printf("Enter the radius = ");
+    scanf("%d",&r);
+
+    area = 3.1416 * r *r;
+    printf("Area of circle is = %lf",area);
+
+ return 0;
+}
+
