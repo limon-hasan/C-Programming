18e62426a8a536784aab666b5f7ce170d7c4ee57
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-26 22:31:26 +0600

    Update AreaOfCircle.c

--- AreaOfCircle.c
+++ AreaOfCircle.c
@@ -12,3 +12,17 @@ int main()
  return 0;
 }
 
+OR,
+
+#include<stdio.h>
+int main()
+{
+    float r, area;
+    printf("Enter the radius = ");
+    scanf("%f",&r);
+
+    area = 3.1416 * r *r;
+    printf("Area of circle is = %f",area);
+
+ return 0;
+}
