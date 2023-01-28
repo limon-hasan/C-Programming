49d1c55c32f1f609be9ca106bc2967dd88261594
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-28 17:40:40 +0600

    Update swap(withoutTemporaryValue).c

--- swap(withoutTemporaryValue).c
+++ swap(withoutTemporaryValue).c
@@ -1,3 +1,5 @@
+(1)------------------------------
+
 #include<stdio.h>
 int main()
 {
@@ -14,3 +16,21 @@ int main()
 
     return 0;
 }
+
+(2)--------------------------
+    
+#include<stdio.h>
+int main()
+{
+    int num1= 18;
+    int num2=38;
+
+    num1=num1+20; //38
+    num2=num2-20; //18
+
+    printf("num1 = %d\n",num1);
+    printf("num2 = %d\n",num2);
+
+    return 0;
+}
+    
