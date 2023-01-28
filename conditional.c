0bfbc9b840ead83044805b5da75b46fb5af887da
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-29 00:01:04 +0600

    Create conditional.c

---
+++ conditional.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+int main()
+{
+    int n = -4;
+if(n < 0) 
+{
+printf("The number is negative\n");
+}
+else if(n > 0) 
+{
+printf("The number is positive\n");
+}
+else 
+{
+printf("The number is zero!\n");
+}
+return 0;
+}
+
+
+OR------------
+  
+  
+#include <stdio.h>
+int main()
+{
+    int n = -4;
+if(n < 0) 
+{
+printf("The number is negative\n");
+}
+else if(n > 0) 
+{
+printf("The number is positive\n");
+}
+else if(n==0)
+{
+printf("The number is zero!\n");
+}
+return 0;
+}  
