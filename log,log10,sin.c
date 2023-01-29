0f5e410ebfab56f79abcd1a459c7f45b78f8bcde
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-29 22:02:57 +0600

    Update log,log10,sin.c

--- log,log10,sin.c
+++ log,log10,sin.c
@@ -1,6 +1,7 @@
 (1)_______________
 
 #include<stdio.h>
+#include<math.h>    // <math.h> eita use na korleo hobe. compiler bujhe nibe je ei library function gula ei HEADER file er vitorei ache 
 int main()
 {
     double x =10.5;
@@ -26,4 +27,16 @@ int main()
 
 (3)_________________
   
-  
+#include<stdio.h>
+#include<math.h>     // <math.h> eita use na korleo hobe. compiler bujhe nibe je ei library function gula ei HEADER file er vitorei ache
+
+int main()
+{
+    double x = 3.9;
+    double result = sin(x);
+    printf("sin(%lf) is = %lf",x,result);
+
+    return 0;
+}
+
+
