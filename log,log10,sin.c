00eec133af7715fd364a09038257757279b50fe5
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-29 21:39:31 +0600

    Create log,log10,sin.c

---
+++ log,log10,sin.c
@@ -0,0 +1,29 @@
+(1)_______________
+
+#include<stdio.h>
+int main()
+{
+    double x =10.5;
+    double result = log(x);
+    printf("log(%lf) is = %lf",x,result);
+
+    return 0;
+}
+
+
+(2)________________
+  
+#include<stdio.h>
+int main()
+{
+    double x =10.5;
+    double result = log10(x);
+    printf("log10(%lf) is = %lf",x,result);
+
+    return 0;
+}
+
+
+(3)_________________
+  
+  
