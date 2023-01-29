a304e13095179d293985e3351ea6571dbd09c460
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-30 01:04:05 +0600

    Add files via upload

---
+++ AssignmentOperator.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int a =3;
+    a+=10; // a = a+10
+    printf("Value is = %d\n",a);
+
+    a*=2; // a=a*2
+    printf("Value is = %d",a);
+
+    return 0;
+}
+
