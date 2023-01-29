a90dfed6eefa334c8f0e1a2953f4246a1b3826c0
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-29 23:28:06 +0600

    Update round,trunc,ceil,floorFunction.c

--- round,trunc,ceil,floorFunction.c
+++ round,trunc,ceil,floorFunction.c
@@ -1,3 +1,5 @@
+(1)____________________________
+    
 #include<stdio.h>
 #include<math.h> 
 
@@ -9,4 +11,48 @@ int main()
 
     return 0;
 }
+
+(2)___________________________________________
+    
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    double x = 10.9;
+    double result = trunc(x);
+    printf("trunc(%lf) is = %lf",x,result);
+
+    return 0;
+}
+
+(3)__________________________________________________
+    
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    double x = 10.9;
+    double result = trunc(x);
+    printf("trunc(%lf) is = %lf",x,result);
+
+    return 0;
+}
+
+(4)______________________________
+    
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+    double x = 9.5;
+    double result = floor(x);                 // floor function er kaj holo jei number ta input dibo sheitar nicher integer number ta show korbe. That means 2.4 dile 2 dekhabe
+    printf("floor(%lf) is = %lf",x,result);
+
+    return 0;
+}
+
+
 
