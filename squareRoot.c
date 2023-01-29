0778b84494661f2ce2e980dbce30fe9ac34047df
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-29 12:01:33 +0600

    Create squareRoot.c

---
+++ squareRoot.c
@@ -0,0 +1,13 @@
+  #include<stdio.h>
+int main()
+{
+    int x = 10;
+    float result = sqrt(x);      //Square Root সবসময় Double type value return করে
+
+    printf("%.4f",result);
+
+}                                                              
+
+
+
+                                        
