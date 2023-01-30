3ba0cce6682675ffa9b638d17158a3bd8dc354fe
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-30 19:56:51 +0600

    Update Decrement.c

--- Decrement.c
+++ Decrement.c
@@ -1,4 +1,4 @@
-(1)________________________________________     
+(1)____________________________________________________________-    
     
 #include<stdio.h>             //Pre Decrement
 int main()
@@ -20,3 +20,36 @@ int main()
     return 0;
 }
 
+(2)_____________________________________________________________________________
+
+#include<stdio.h>                 //Post Decrement
+int main()
+{
+    int x=10;
+    x--;
+    printf("x = %d\n",x);
+    return 0;
+}
+
+Or___________________________________
+
+#include<stdio.h>                  //Post Decrement
+int main()
+{
+    int x=10;
+    int y = x--;
+    printf("x = %d\n",x);
+    return 0;
+}
+
+Or_________________________________
+
+#include<stdio.h>                    //Post Decrement
+int main()
+{
+    int x=10;
+    int y = x--; //y=9
+    printf("x = %d\n",x);
+    printf("y = %d\n",y);
+    return 0;
+}
