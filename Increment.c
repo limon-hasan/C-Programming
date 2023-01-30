54edd9e549585da149f3829fb5143b159d742feb
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-30 19:27:42 +0600

    Update Increment.c

--- Increment.c
+++ Increment.c
@@ -1,9 +1,55 @@
-#include<stdio.h>       //Pre Increment
+(1)___________________________________
+    
+#include<stdio.h>       //Post Increment
 int main()
 {
     int x=10;
     int y = x++; //y=10
     printf("x = %d\n",x);
     printf("y = %d\n",y);
+    return 0;
+}
+
+Or________________
+
+#include<stdio.h>               //Post Increment
+int main()
+{
+    int x=10;
+    x++;
+    printf("x = %d\n",x);
+    return 0;
+}
+
+(2)___________________________________________
+    
+#include<stdio.h>                  //Pre Increment                                                            
+int main()
+{
+    int x=10;
+    int y = ++x; //y=11
+    printf("x = %d\n",y);
+    return 0;
+}
+
+Or__________________
+
+#include<stdio.h>                 //Pre Increment
+int main()
+{
+    int x=10;
+    int y = ++x; //y=11
+    printf("x = %d\n",y);
+    return 0;
+}
+
+Or________________
+
+#include<stdio.h>            //Pre Increment
+int main()
+{
+    int x=10;
+    ++x;
+    printf("x = %d\n",x);
     return 0;
 }
