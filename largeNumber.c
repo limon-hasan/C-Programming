51f2aaa4d9e9257c52382189d1731928092656b1
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-31 16:17:34 +0600

    Update largeNumber.c

--- largeNumber.c
+++ largeNumber.c
@@ -50,7 +50,7 @@ int main()
     }
     else if(num1<num2)                  // Ekhane else-if na diye jodi shudhu if ditam tahole (num1>num2) jodi true hoito tokhon abar nicher line e ashto. Jotobar if thakbe totobar e compiler check korbe. Ar jodi else-if ditam tahole prothom statement e (num1>num2) jodi true hoito tahole ar nicher line e ashto na, prothom statement ei program shesh hoye jaito. else ba else-if thakle prothom statement true hole niche ar ashbe na, ar jodi prothom ta true na hoy tahole nicher statement e ashbe. Ar if thakle prothom tao check korbe shathe second tao check korbe, jotobar thakbe totobar check korbe
     {
-        printf("Large is = %d\n",num2);
+        printf("Large is = %d\n",num2); // else shobshomoy program er shesh e thake, program e ekta matro else thakte pare. Kintu proyonjon onujayi if, else-if jotogula iccha totogulause kora jabe
     }
     else
     {
