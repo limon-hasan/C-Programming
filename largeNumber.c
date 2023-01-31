c65f86b7e3d6a2dd1a7f2018d5e2baf1c5b03d94
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-01-31 15:49:48 +0600

    Update largeNumber.c

--- largeNumber.c
+++ largeNumber.c
@@ -1,3 +1,5 @@
+(1)___________________________________________________________________________
+
 #include<stdio.h>
 int main()
 {
@@ -27,3 +29,33 @@ int main()
 
     return(0);
 }
+
+
+(2)_____________________________________________________________________________________
+    
+#include<stdio.h>
+int main()
+
+{
+    int num1,num2;
+    printf("Enter First number = ");
+    scanf("%d",&num1);
+
+    printf("Enter Second number = ");
+    scanf("%d",&num2);
+
+    if(num1>num2)
+    {
+        printf("Large is = %d\n",num1);
+    }
+    else if(num1<num2)                  // Ekhane else-if na diye jodi shudhu if ditam tahole (num1>num2) jodi true hoito tokhon abar nicher line e ashto. Jotobar if thakbe totobar e compiler check korbe. Ar jodi else-if ditam tahole prothom statement e (num1>num2) jodi true hoito tahole ar nicher line e ashto na, prothom statement ei program shesh hoye jaito. else ba else-if thakle prothom statement true hole niche ar ashbe na, ar jodi prothom ta true na hoy tahole nicher statement e ashbe. Ar if thakle prothom tao check korbe shathe second tao check korbe, jotobar thakbe totobar check korbe
+    {
+        printf("Large is = %d\n",num2);
+    }
+    else
+    {
+        printf("Both are equal\n");
+    }
+    return 0;
+}
+    
