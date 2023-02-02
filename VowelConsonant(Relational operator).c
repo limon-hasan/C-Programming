60f521c29e6b80ab644f3a39589b201e0816f370
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-02-02 22:34:28 +0600

    Add files via upload

---
+++ VowelConsonant(Relational operator).c
@@ -0,0 +1,34 @@
+//Enter a letter to check vowel or consotant
+#include<stdio.h>
+int main()
+{
+    char ch;
+    printf("Enter a letter = ");
+    scanf("%c",&ch);
+
+    if(ch=='a')
+    {
+        printf("vowel\n");
+    }
+    else if(ch=='e')
+    {
+        printf("vowel\n");
+    }
+    else if(ch=='i')
+    {
+        printf("vowel\n");
+    }
+    else if(ch=='o')
+    {
+        printf("vowel\n");
+    }
+    else if(ch=='u')
+    {
+        printf("vowel\n");
+    }
+    else
+    {
+        printf("consonant\n");
+    }
+    return 0;
+}
