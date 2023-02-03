39f2309156a7825bac09dc8751b4be0497fe9344
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-02-03 21:41:46 +0600

    Update Vowel,Consonant(LogicalOperator).c

--- Vowel,Consonant(LogicalOperator).c
+++ Vowel,Consonant(LogicalOperator).c
@@ -3,7 +3,7 @@
 int main()
 {
     char ch;
-    printf("Enter a letter = ");
+    printf("Enter a letter = ");                     // && = Logical AND, || = Logical OR, ! = Logical NOT
     scanf("%c",&ch);
 
     if(ch=='a'|| ch=='A'|| ch=='e'|| ch=='E'|| ch=='i'|| ch=='I' || ch=='o'|| ch=='O'|| ch=='u'|| ch=='U')
