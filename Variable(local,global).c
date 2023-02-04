41c9f0a1a035a06f158126a413d88fceb1e1f140
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-02-04 19:32:14 +0600

    Update Variable(local,global).c

--- Variable(local,global).c
+++ Variable(local,global).c
@@ -1,15 +1,26 @@
+(1)____________________________________
+    
 #include<stdio.h>
 int main()
 {
-    int a=10;
+    int a=10;   // a = local variable
     printf("Inside the main function a= %d\n",a);
 
 }
 
+(2)________________________________________
+ 
+    
+#include<stdio.h>
+int a=10;      // a = global variable
+int main()
+{
+    printf("Inside the main function a= %d\n",a);
+    display();
 
+}
 
-
-
-
-
-
+void display()
+{
+    printf("Inside the display function a= %d\n",a);
+}    
