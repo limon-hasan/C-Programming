3955d2d548eaabaaa12c9a0f27230a3ce9ece97f
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-02-08 23:52:33 +0600

    Update large,small_(ConditionalOperator).c

--- large,small_(ConditionalOperator).c
+++ large,small_(ConditionalOperator).c
@@ -9,5 +9,20 @@ int main()
     printf("The large number is = %d\n",AB);
 
     return 0;
+}
 
+
+Or,------------------------------------
+
+#include<stdio.h>
+int main()
+{
+    int a,b,AB;
+    printf("Enter two numbers = ");
+    scanf("%d %d",&a,&b);
+
+    AB = a<b ? a : b;
+    printf("The small number is = %d\n",AB);
+
+    return 0;
 }
