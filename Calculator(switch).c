7e1481525575bd1ffe209d71fa62f92ebfe8f1ec
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-02-08 19:38:17 +0600

    Update Calculator(switch).c

--- Calculator(switch).c
+++ Calculator(switch).c
@@ -1,7 +1,10 @@
+(1)_____________________________________________________
+    
+    
 #include<stdio.h>
 int main()
 {
-    double num1,num2;
+    double num1,num2;                  // Ekhane double type newa tai convenient, float type o newa jete pare
     char op;
 
     printf("Enter an operator (+,-,*,/) = ");              // Ekhane character print agee na niye jodi numbers print agee nei tahole thikmoto run korbe na, shudhu numbers print korbe, character korbe na
@@ -18,7 +21,7 @@ int main()
         printf("%lf - %lf = %lf\n",num1,num2,num1-num2);
         break;
     case '*':
-        printf("%lf * %lf = %lf\n",num1,num2,num1*num2);
+        printf("%lf * %lf = %lf\n",num1,num2,num1*num2);         
         break;
     case '/':
         printf("%lf / %lf = %lf\n",num1,num2,num1/num2);
@@ -27,5 +30,45 @@ int main()
         printf("Not a valid operator\n");
     }
 
+    return 0;
+}
+
+
+(2)______________________________________________________
+
+    
+#include<stdio.h>
+int main()
+{
+    float num1,num2,p,q,r,t;
+    char ch;
+
+    printf("Enter any operator (+,-,*,/) = ");
+    scanf("%c",&ch);
+    printf("Enter two numbers = ");
+    scanf("%f %f", &num1,&num2);
+
+    p=num1+num2;
+    q=num1-num2;
+    r=num1*num2;
+    t=num1/num2;
+
+    switch(ch)
+    {
+        case '+':
+        printf("%f + %f = %f\n",num1,num2,p);
+        break;
+        case '-':
+        printf("%f - %f = %f\n",num1,num2,q);
+        break;
+        case '*':
+        printf("%f * %f = %f\n",num1,num2,r);
+        break;
+        case '/':
+        printf("%f / %f = %f\n",num1,num2,t);
+        break;
+        default:
+        printf("Not a valid operator\n");
+    }
     return 0;
 }
