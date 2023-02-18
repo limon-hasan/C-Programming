bae8a64821dc198e9290eaf26fd0894a560725ee
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-02-19 03:35:24 +0600

    Create loop(even,odd Numbers-100).c

---
+++ loop(even,odd Numbers-100).c
@@ -0,0 +1,25 @@
+1----------------------
+  
+  
+#include<stdio.h>               // 1-100 porjonto even numbers 
+int main()
+{
+    int x;
+    for(x=2; x<=100; x=x+2)
+        printf("%d\n",x);
+
+
+}
+
+2------------------------
+  
+  
+#include<stdio.h>
+int main()
+{
+    int x;
+    for(x=1; x<=100; x=x+2)
+        printf("%d\n",x);
+
+
+}
