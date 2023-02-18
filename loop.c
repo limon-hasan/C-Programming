1305a23b9cb6c48f299edd078508d126b5fcad52
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-02-19 02:58:35 +0600

    Create loop.c

---
+++ loop.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    int i;
+    for(i=1; i<=100; i++)  // for(initialization; condition; counter)
+                          //{ statement eker odhik thakle second bracket dibo}
+      
+    {                  
+     printf("C Programming\n");
+     printf("%d\n",i);
+
+    }
+
+
+}
