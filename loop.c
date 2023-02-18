f060965cef1311bea2ba98fb238d0b21ca474a84
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-02-19 03:09:00 +0600

    Update loop.c

--- loop.c
+++ loop.c
@@ -5,13 +5,32 @@
 int main()
 {
     int i;
-    for(i=1; i<=100; i++)  // for(initialization; condition; counter)
+    for(i=1; i<=100; i++)  // For Loop e 3 ta part thake. for(initialization; condition; counter)
                           //{ statement eker odhik thakle second bracket dibo}
       
     {                  
      printf("C Programming\n");
      printf("%d\n",i);
 
+    }
+
+
+}
+
+
+
+2-(while loop)-----------------------------------------
+    
+
+#include<stdio.h>
+int main()
+{
+    int i=1;
+    while(i<=10)    // while loop e shudhu condition thake
+
+    {
+        printf("%d\n",i);
+        i++;
     }
 
 
