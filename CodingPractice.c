145580a07938eb194ea2e6630a95e84637e9dcfa
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-02-23 19:50:42 +0600

    Update CodingPractice.c

--- CodingPractice.c
+++ CodingPractice.c
@@ -1,3 +1,6 @@
+If...Else Example Codes------------------------------------------------------
+
+
 #include<stdio.h>
 int main()
 {
@@ -8,8 +11,30 @@ int main()
         printf("2\n");
     if(x--)
         printf("3\n");
-    if(x--)
+    if(x--)        // x=-1
         printf("None\n");
 
-    printf("%d",x);
+    printf("%d",x);  // x=-2
+}                                            //  Decrement hole First e value check korbe, pore jeta kome othoba bare oita check korbe. 
+                                              // Increment e first ei value barbe othoba kombe
+
+
+Or-------------------------------------------
+    
+    
+#include<stdio.h>
+int main()
+{
+    int x=2;
+    if(x--)
+        printf("1\n");
+    if(x--)
+        printf("2\n");
+    if(x--)
+        printf("3\n");
+    if(x++)          //x=-1
+        printf("None\n");
+
+    printf("%d",x); // x=0
 }
+    
