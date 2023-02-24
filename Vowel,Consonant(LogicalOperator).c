9ffa05fe2e12d37070a9424b52dadf518c38bc52
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-02-24 23:42:40 +0600

    Update Vowel,Consonant(LogicalOperator).c

--- Vowel,Consonant(LogicalOperator).c
+++ Vowel,Consonant(LogicalOperator).c
@@ -1,19 +1,22 @@
 //Enter a letter to check vowel or consotant
+                 
 #include<stdio.h>
 int main()
 {
+                                              // && = Logical AND, || = Logical OR, ! = Logical not
     char ch;
-    printf("Enter a letter = ");                     // && = Logical AND, || = Logical OR, ! = Logical NOT
+    printf("Enter a character  = ");
     scanf("%c",&ch);
 
-    if(ch=='a'|| ch=='A'|| ch=='e'|| ch=='E'|| ch=='i'|| ch=='I' || ch=='o'|| ch=='O'|| ch=='u'|| ch=='U')
-    {
-        printf("vowel\n");
-    }
-
+    if(ch=='a' || ch=='e' || ch=='i' || ch=='e' || ch=='u')
+        printf("This is vowel");
+    else if(ch=='A' || ch=='I' || ch=='O' || ch=='E' || ch=='U')
+        printf("This is vowel");
+    else if(ch>='B' && ch<='Z')
+        printf("This is consonant");
+    else if(ch>='b' && ch<='z')
+        printf("This is consonant");
     else
-    {
-        printf("consonant\n");
-    }
-    return 0;
+        printf("This is not valid");
+    getch();
 }
