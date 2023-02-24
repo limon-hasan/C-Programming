37cbe8c28fb60b2e507845314ed9c7a49d53dc64
Maksudul hasan Limon <120485933+Lim13on@users.noreply.github.com>
2023-02-24 19:58:36 +0600

    Update Leap Year.c

--- Leap Year.c
+++ Leap Year.c
@@ -20,3 +20,21 @@ int main()
     getch();
 }
 
+
+Or---------------------------------
+    
+    
+#include<stdio.h>
+int main()
+{
+    int year;
+    printf("Enter a year = ");
+    scanf("%d",&year);
+    
+    if(year%400==0 || year%4==0 && year%100!=0)
+        printf("Leap Year");
+
+    else
+        printf(" Not Leap Year");
+    getch();
+}
