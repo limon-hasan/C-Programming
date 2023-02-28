bf85a65df12d4f739bb8d9abb694adc696ca7a28
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-01 03:26:34 +0600

    Create Sum_of_digits.c

---
+++ Sum_of_digits.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int num,temp,sum=0,r;
+    printf("Enter a number = ");  // num=423
+    scanf("%d",&num);
+
+    temp=num;
+
+    while(temp!=0)
+    {
+        r= temp % 10;
+        sum = sum + r;
+        temp= temp/10;
+    }
+    printf("Sum of digits = %d\n",sum); // sum=9
+
+    return 0;
+}
