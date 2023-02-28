64ce0b90bd4c0a6d4fa55319370ad09189a07961
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-02-28 23:21:02 +0600

    Create Factorial.cpp

---
+++ Factorial.cpp
@@ -0,0 +1,18 @@
+// Finding factorial
+
+#include<stdio.h>
+int main()
+{
+    int n,i,fact =1;
+    printf("Enter a number : ");
+    scanf("%d,",&n);   // n=4
+
+    for(i=1; i<=n; i++)
+    {
+        fact=fact*i;
+    }
+
+    printf("Factorial = %d\n",fact);
+
+    return 0;
+}
