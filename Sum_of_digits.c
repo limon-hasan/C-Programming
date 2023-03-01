4636557f81363834a4ac44a87af717b868e5c9a0
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-01 20:09:09 +0600

    Update Sum_of_digits.c

--- Sum_of_digits.c
+++ Sum_of_digits.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main()
 {
-    int num,temp,sum=0,r;
+    int num,temp,sum=0,r;         // Shudhu sum of digits er khetre temp variable na niye num variable diye krorleo hobe. 
+                                  // karon ekhane pore kothao num variable er theke kono input newa lagtese na, jeta Palindrome othoba Armstron number checkkorar khetre last e If conditon e num variable er shathe onno variable check korte hoy ejonno
     printf("Enter a number = ");  // num=423
     scanf("%d",&num);
 
