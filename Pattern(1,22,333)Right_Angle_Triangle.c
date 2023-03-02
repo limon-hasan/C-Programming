b1d595fdb327feba4794aeb641a284697b2cefc1
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-03 04:06:51 +0600

    Create Pattern(1,22,333)Right_Angle_Triangle.c

---
+++ Pattern(1,22,333)Right_Angle_Triangle.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+    while(1)
+    {
+        int n,row,col;
+        printf("Enter N = ");
+        scanf("%d",&n);  // n = 4
+
+        for(row=1; row<=n; row++)
+        {
+            for(col=1; col<=row; col++)
+            {
+                printf("%d ",row);
+            }
+            printf("\n");
+        }
+    }
+        return 0;
+    }
+
