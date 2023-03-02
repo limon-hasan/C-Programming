5dcd59adcee6dc092c9014d05eeb99521a6d3dca
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-02 12:16:46 +0600

    Update armstrong_nmbr(1to1000).cpp

--- armstrong_nmbr(1to1000).cpp
+++ armstrong_nmbr(1to1000).cpp
@@ -1,29 +1,26 @@
 // 1 theke 1000 porjonto jei number gula Armstrong sheigula print korte hobe
-
-#include<stdio.h>                  // Armstrong number holo ekta mumber er prottek digit er cube er total sum oi number er same hobe
-int main()
+   
+#include<stdio.h> 
+int main()              
 {
-    int i,initialNum,finalNum,r,sum=0,temp;
-    printf("Initial value = ");
-    scanf("%d",&initialNum);     // num=153
-    printf("Final value = ");
-    scanf("%d",&finalNum);
+        int i,temp,initialNum,finalNum,sum=0,r;
+        printf("Enter initial value = ");
+        scanf("%d",&initialNum);
+        printf("Enter final value = ");
+        scanf("%d",&finalNum);
 
-    for(i=initialNum; i<=finalNum; i++)
-    {
-        temp=i;
-
-        while(temp!=0)
+        for(i=initialNum; i<=finalNum; i++)
         {
-            r=temp%10;
-            sum=sum+r*r*r;
-            temp=temp/10;
+            temp=i;
+            while(temp!=0)
+            {
+                r = temp%10;
+                sum = sum + r*r*r;
+                temp = temp/10;
+            }
+
+            if(sum==i)
+                printf("Armstrong Number = %d\n",i);
+            sum = 0;         // Ekhane sum zero diye nite hobe karon printf e armstrong number ta showkorar por jokhon abar upore for loop e jabe oikhan theke pore loop er vitor diye cholar shomoy last jeita sum er value chilo oita diye calculation korbe mane zero diye korbe na, mane sum er value ta bere jabe onek ejonno prottek bar print korar por upore jawar age sum er value zero diye nibo jeno pore calculation er khetre prothome zero diye calculate kore
         }
-        printf("%d\n",sum);
-
-        if(i==sum)
-            printf("%d",i);
-    }
-
-    sum=0;
 }
