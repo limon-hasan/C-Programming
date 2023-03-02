c87ea64fc1e4ddd430fb48eb308d39fda3a1e3ba
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-03 03:43:10 +0600

    Update Pattern(1,12,123)Right_Angle_Triangle.c

--- Pattern(1,12,123)Right_Angle_Triangle.c
+++ Pattern(1,12,123)Right_Angle_Triangle.c
@@ -1,3 +1,5 @@
+(1)--------------------------------------------
+
 #include<stdio.h>
 int main()
 {
@@ -16,3 +18,27 @@ int main()
 
     return 0;
 }
+
+
+(2)----------------------------------------
+    
+#include<stdio.h>
+int main()
+{
+    int n,row,col;
+    printf("Enter N = ");
+    scanf("%d",&n);  // n = 4
+
+    for(row=1; row<=n; row++)
+    {
+        for(col=1; col<=row; col++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
+      
