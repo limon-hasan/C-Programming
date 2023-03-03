922ff992892656aa883e0d8321d03cf920024da2
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-04 00:07:41 +0600

    Create Pattern-3(101,10,1)Left_Side_Space.c

---
+++ Pattern-3(101,10,1)Left_Side_Space.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+    int n,row,col;
+    printf("Enter N = ");
+    scanf("%d",&n);  // n = 4
+
+    for(row=n; row>=1; row--)
+    {
+        // printing space
+        for(col=1; col<=n-row; col++) // Ekhane space er value hobe joto number row te ashi n theke oi number ta minus korbo
+        {
+            printf(" ");
+        }
+
+        // printing number
+        for(col=1; col<=row; col++)
+        {
+            printf("%d",col%2);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
