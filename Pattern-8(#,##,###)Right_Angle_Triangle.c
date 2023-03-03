5030079d48a5e244e171bbc838b2025e65c002b8
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-03 11:42:39 +0600

    Create Pattern-8(#,##,###)Right_Angle_Triangle.c

---
+++ Pattern-8(#,##,###)Right_Angle_Triangle.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+    int n,row,col;
+    printf("Enter N = ");
+    scanf("%d",&n);  // n = 4
+
+    for(row=1; row<=n; row++)
+    {
+        for(col=1; col<=row; col++)
+        {
+            printf("# ");
+        }
+        printf("\n");
+    }
+
+
+    return 0;
+}
