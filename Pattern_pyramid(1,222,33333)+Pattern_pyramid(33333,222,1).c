a0b947a93e15ee1c49dafcdbab5698c24bd4779a
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-04 05:17:08 +0600

    Create Pattern_pyramid(1,222,33333)+Pattern_pyramid(33333,222,1).c

---
+++ Pattern_pyramid(1,222,33333)+Pattern_pyramid(33333,222,1).c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+int main()
+{
+    int n,row,col;
+    printf("Enter N = ");
+    scanf("%d",&n);  // n = 3
+
+    for(row=1; row<=n; row++)
+    {
+        for(col=1; col<=n-row; col++)
+        {
+            printf(" ");
+        }
+
+        for(col=1; col<=(row*2)-1; col++)
+        {
+            printf("%d",row);
+        }
+        printf("\n");
+    }
+
+    for(row=n-1; row>=1; row--)  // n-1=3-1=2
+    {
+        for(col=1; col<=n-row; col++)
+        {
+            printf(" ");
+        }
+
+        for(col=1; col<=(row*2)-1; col++)
+        {
+            printf("%d",row);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
