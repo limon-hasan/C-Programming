00f0e3f4729f306d16dee09a39f74317999683e2
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-03 11:29:44 +0600

    Create Pattern-5(A,AB,ABC)Right_Angle_Triangle.c

---
+++ Pattern-5(A,AB,ABC)Right_Angle_Triangle.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    while(1)
+    {
+        int n,row,col;
+        printf("Enter N = ");
+        scanf("%d",&n);  // n = 4
+
+        for(row=1; row<=n; row++)
+        {
+            for(col=1; col<=row; col++)
+            {
+                printf("%c ",col+64);
+            }
+            printf("\n");
+        }
+    }
+        return 0;
+    }
