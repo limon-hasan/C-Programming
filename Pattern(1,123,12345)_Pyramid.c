e37b9f7397edf6f66913d294fc42d34eedd68747
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-04 04:34:37 +0600

    Create Pattern(1,123,12345)_Pyramid.c

---
+++ Pattern(1,123,12345)_Pyramid.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+    int n,row,col;
+    printf("Enter N = ");
+    scanf("%d",&n);  // n = 4
+
+    for(row=1; row<=n; row++)
+    {
+        for(col=1; col<=n-row; col++)
+        {
+            printf(" ");
+        }
+
+        for(col=1; col<=(row*2)-1; col++)
+        {
+            printf("%d",col);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
