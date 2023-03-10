2c1517b1baf77f7d73d3d3a9dbe8634284f819fd
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-11 03:20:37 +0600

    Create

---
+++ Array_declaration.c
@@ -0,0 +1,43 @@
+// Array definition
+/*
+Array declare
+datatype array_name [array_size];
+*/
+// Array initialize
+
+#include<stdio.h>
+int main()
+{
+    int num[] = {10,20,30,40,50},sum;
+
+
+    sum = num[0]+num[1]+num[2]+num[3]+num[4];
+    printf("The sum is %d\n",sum);
+
+}
+
+
+Or,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+
+// Array definition
+/*
+Array declare
+datatype array_name [array_size];
+*/
+// Array initialize
+
+#include<stdio.h>
+int main()
+{
+    int num[5],sum;
+    num[0]=10;
+    num[1]=20;
+    num[2]=30;
+    num[3]=40;
+    num[4]=50;
+
+    sum = num[0]+num[1]+num[2]+num[3]+num[4];
+    printf("The sum is %d\n",sum);
+
+}
