933a0bf685080fbe9f5fea1a40b4e442fd9eaccf
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-11 16:36:45 +0600

    Update Array(Sum&Average).c

--- Array(Sum&Average).c
+++ Array(Sum&Average).c
@@ -41,3 +41,24 @@ int main()
     printf("The average is %.2f",(float)sum/5);
 
 }
+
+
+Or,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+
+#include<stdio.h>
+int main()
+{
+    int a[5],sum=0,i;
+    printf("Enter five numbers = ");
+    scanf("%d %d %d %d %d",&a[0],&a[1],&a[2],&a[3],&a[4]);
+
+    for(i=0; i<5; i++)
+    {
+        sum = sum + a[i];
+    }
+
+    printf("The sum is %d\n",sum);
+    printf("The average is %.2f",(float)sum/5);
+
+}
