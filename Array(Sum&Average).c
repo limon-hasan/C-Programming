ae17fe2418d17af889049b841706306d00ee0932
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-12 08:05:33 +0600

    Update Array(Sum&Average).c

--- Array(Sum&Average).c
+++ Array(Sum&Average).c
@@ -62,3 +62,29 @@ int main()
     printf("The average is %.2f",(float)sum/5);
 
 }
+
+
+Or,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+
+#include<stdio.h>                  // Ekhane shobgula number e sum korbo na, user er iccha moto jotogula number sum korte chay totogula korbo
+int main()
+{
+    int a[100],sum=0,i,n;
+    printf("How many numbers = ");
+    scanf("%d",&n);  // n=10
+
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0; i<n; i++)
+    {
+        sum = sum + a[i];
+    }
+
+    printf("The sum is %d\n",sum);
+    printf("The average is %.2f",(float)sum/n);
+
+}
