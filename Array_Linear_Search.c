cc6072cb75a501afd8eeea74e9d969cc161aec21
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-15 13:20:37 +0600

    Create

---
+++ Array_Linear_Search.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+int main()
+{
+while(1)
+{
+    {
+        int a[30],i,n,value;
+        printf("Enter how many numbers = ");
+        scanf("%d",&n);
+        printf("Enter the value = ");
+        scanf("%d",&value);
+
+        for(i=0; i<n; i++)
+        {
+            scanf("%d",&a[i]);
+        }
+
+        for(i=0; i<n; i++)
+        {
+            if(value==a[i])
+                break;
+        }
+        if(value==a[i])
+            printf("Found\n");
+        else
+            printf("Not found\n");
+    }
+
+}
+}
