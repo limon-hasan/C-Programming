92fe3682a60e11b8b064a4639618d8cabc608fdf
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-15 14:33:48 +0600

    Update Array_Linear_Search.c

--- Array_Linear_Search.c
+++ Array_Linear_Search.c
@@ -28,3 +28,37 @@ while(1)
 
 }
 }
+
+
+Or,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+
+#include<stdio.h>
+int main()
+{
+    while(1)
+    {
+        {
+            int a[30],i,n,value;
+            printf("Enter how many numbers = ");
+            scanf("%d",&n);
+            printf("Enter the value = ");
+            scanf("%d",&value);
+
+            for(i=0; i<n; i++)
+            {
+                scanf("%d",&a[i]);
+            }
+
+            for(i=0; i<n; i++)
+            {
+                if(value==a[i])
+                    break;
+            }
+            if(value!=a[i])
+                printf("Not Found\n");
+            else
+                printf("Found\n");
+        }
+    }
+}
