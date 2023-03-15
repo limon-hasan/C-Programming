9c7e87b6bc8a003972e3c45625764d1d85f58d41
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-15 18:55:32 +0600

    Create

---
+++ Array_Copy_elements_array1 to array2.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main()
+{
+    int array1[5]= {10,20,30,40,50},array2[5],i;
+
+    printf("Array1 : ");
+    for(i=0; i<5; i++)
+    {
+        printf("%d ",array1[i]);
+    }
+    printf("\n");
+    
+    // copying all the elements from array1 to array2
+
+    for(i=0; i<5; i++)
+    {
+        array2[i]=array1[i];
+    }
+
+    printf("Array2 : ");
+    for(i=0; i<5; i++)
+    {
+        printf("%d ",array2[i]);
+    }
+    
+    getch();
+}
