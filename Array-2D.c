c200025eaaf4ee6fa7a1c21f2dbb96061128a305
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-15 23:46:05 +0600

    Update Array-2D.c

--- Array-2D.c
+++ Array-2D.c
@@ -15,3 +15,25 @@ int main()
 
     return 0;
 }
+
+
+Or,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+
+#include<stdio.h>
+int main()
+{
+   int A[3][4]={  {5,6,7},{9,10,12},{14,15,16}  };
+   int i=3,j=4;
+
+   for(i=0; i<3; i++)
+   {
+       for(j=0; j<4; j++)
+       {
+           printf("%d ",A[i][j]);
+       }
+       printf("\n");
+   }
+
+    return 0;
+}
