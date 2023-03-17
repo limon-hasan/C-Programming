c05a03e9c3207974684e8cd7a6b8cc97679ab7c6
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-17 18:09:36 +0600

    Create

---
+++ Array_Matrix_Addition & Substraction.c
@@ -0,0 +1,69 @@
+#include<stdio.h>
+int main()
+{
+    while(1)
+    {
+        int A[10][10],B[10][10],C[10][10],no_rows,no_cols,i,j;
+
+        printf("Enter rows = ");
+        scanf("%d",&no_rows);
+        printf("Enter cols = ");
+        scanf("%d",&no_cols);
+
+        printf("\n");
+
+        printf("Enter elements for A matrix :\n");
+        for(i=0; i<no_rows; i++)
+        {
+            for(j=0; j<no_cols; j++)
+            {
+                printf("A[%d][%d] = ",i,j);
+                scanf("%d",&A[i][j]);
+            }
+            printf("\n");
+        }
+
+        printf("\nEnter elements for B matrix :\n");
+        for(i=0; i<no_rows; i++)
+        {
+            for(j=0; j<no_cols; j++)
+            {
+                printf("B[%d][%d] = ",i,j);
+                scanf("%d",&B[i][j]);
+            }
+            printf("\n");
+        }
+
+        printf("\nA = ");
+        for(i=0; i<no_rows; i++)
+        {
+            for(j=0; j<no_cols; j++)
+            {
+                printf("\t%d ",A[i][j]);
+            }
+            printf("\n");
+        }
+
+        printf("B = ");
+        for(i=0; i<no_rows; i++)
+        {
+            for(j=0; j<no_cols; j++)
+            {
+                printf("\t%d ",B[i][j]);
+            }
+            printf("\n");
+        }
+
+        printf("\nC = ");
+        for(i=0; i<no_rows; i++)
+        {
+            for(j=0; j<no_cols; j++)
+            {
+                C[i][j] = A[i][j]+B[i][j];
+                printf("\t%d ",C[i][j]);
+            }
+            printf("\n");
+        }
+    }
+    getch();
+}
