e435d738c3e13d4d56fea0c09eb265d7f642121b
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-21 02:22:21 +0600

    Create Array_Exercise-3.c

---
+++ Array_Exercise-3.c
@@ -0,0 +1,32 @@
+#include<stdio.h>        // Check Second highest value
+#include<limits.h>
+int main()
+{
+    int a[100],i,n,l1,l2;  // l1 = Highest value, l2 = second highest value
+    printf("How many numbers = ");
+    scanf("%d",&n);
+
+    printf("Enter the numbers : ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    l1 = INT_MIN;
+    l2 = INT_MIN;
+
+    for(i=0; i<n; i++)
+    {
+        if(a[i]>l1)
+        {
+            l2 = l1;
+            l1 = a[i];
+        }
+        else if(a[i]<l1 && a[i]>l2)
+            l2 = a[i];
+    }
+
+    printf("\nFirst Largest value = %d",l1);
+    printf("\nSecond Largest value = %d",l2);
+
+}
