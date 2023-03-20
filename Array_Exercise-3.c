28ab621e4d9b1048a36ab67f4f267cd5ee450f64
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-21 03:48:28 +0600

    Update Array_Exercise-3.c

--- Array_Exercise-3.c
+++ Array_Exercise-3.c
@@ -30,3 +30,40 @@ int main()
     printf("\nSecond Largest value = %d",l2);
 
 }
+
+
+Or,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+
+#include<stdio.h>        // Check Second smallest value
+#include<limits.h>
+int main()
+{
+    int a[100],i,n,l1,l2;  // l1 = Smallest value, l2 = Second smallest value
+    printf("How many numbers = ");
+    scanf("%d",&n);
+
+    printf("Enter the numbers : ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    l1 = INT_MAX;
+    l2 = INT_MAX;
+
+    for(i=0; i<n; i++)
+    {
+        if(a[i]<l1)
+            {
+                l2 = l1;
+            l1 = a[i];
+            }
+            else if(a[i]>l1 && a[i]<l2)
+                l2 = a[i];
+    }
+
+    printf("\nFirst Smallest value = %d",l1);
+    printf("\nSecond smallest value = %d",l2);
+
+}
