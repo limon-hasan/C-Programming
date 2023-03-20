6a0c0504eef63d6e978422a0cb1333972f2aa70a
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-20 17:55:10 +0600

    Update Strong_number.c

--- Strong_number.c
+++ Strong_number.c
@@ -23,3 +23,32 @@ int main()
         printf("%d is Not a Strong Number",num);
     return 0;
 }
+
+
+Or,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+
+#include <stdio.h>   // frequency of a digit bolte bujhay suppose ekta number jodi 122355 hoy ebong ei digit gular moddhe jodi bole 5 kotobar ase tahole oita hocche 5 er frequency
+int main()
+{
+    int num,r,temp,i,freq;
+    printf("Enter a number = ");
+    scanf("%d",&num);  // num = 122355
+
+    for(i=0; i<10; i++)
+    {
+        freq=0;
+        temp=num;
+        while(temp!=0)
+        {
+            r = temp%10;
+            if(i==r)
+            {
+                freq++;
+            }
+            temp=temp/10;
+        }
+        printf("The frequency of %d = %d\n",i,freq);
+    }
+
+}
