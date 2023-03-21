7312db9b5b7651ba81aa0d7f717e817f3906de03
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-21 20:10:14 +0600

    Update Array_Exercise-5.c

--- Array_Exercise-5.c
+++ Array_Exercise-5.c
@@ -19,3 +19,32 @@ int main()
         total_score=0;
     }
 }
+
+
+Or,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+
+// Exercise-5
+
+#include<stdio.h>
+int main()
+{
+    int marks[5][5],i,total_score=0;
+
+    for(i=0; i<5; i++)
+    {
+        printf("Student %d : ",i+1);
+        scanf("%d %d %d %d %d",&marks[i][0],&marks[i][1],&marks[i][2],&marks[i][3],&marks[i][4]);
+    }
+
+    for(i=0; i<5; i++)
+    {
+        total_score = total_score+marks[i][0]+ marks[i][1]+marks[i][2]+marks[i][3]+marks[i][4];
+        printf("Student %d : Total score = %d, ",i+1,total_score);
+        printf("Average = %d\n",total_score/5);
+        total_score=0;
+    }
+
+
+
+}
