098d27590ff773a7a1020744d0ac2b30d68e96c4
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-22 22:52:34 +0600

    Update Function_Subsraction.c

--- Function_Subsraction.c
+++ Function_Subsraction.c
@@ -16,3 +16,31 @@ int main()
     sum(100,200,300);
     sub(200,50);
 }
+
+
+Or----------------------------------------------------------------
+    
+    
+#include<stdio.h>
+
+int sum(int a, int b, int c)
+{
+    return a+b+c;
+}
+
+int sub(int a, int b)
+{
+    return a-b;
+}
+
+int main()
+{
+    int result = sum(10,20,30);
+    result = sum(100,200,300);
+    result = sub(200,50);
+
+    printf("The Sum is = %d\n",sum(10,20,30));
+    printf("The Sum is = %d\n",sum(100,200,300));
+    printf("The Sub is = %d\n",sub(200,50));
+}
+    
