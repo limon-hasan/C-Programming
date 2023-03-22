6ed1b064825964decd76156fd9cbf72bdf647f54
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-22 17:50:26 +0600

    Update Function_Sum_of_2-3 numbers.c

--- Function_Sum_of_2-3 numbers.c
+++ Function_Sum_of_2-3 numbers.c
@@ -81,4 +81,27 @@ int sum(int a,int b)
 }
 
 
+Or,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+
+
+#include<stdio.h>
+
+int sum(int a,int b)
+{
+    int total = a+b;
+    return total;
+}
+
+int main()
+{
+    int num1,num2;
+
+    int result = sum(5,6);
+
+    printf("The SUM is = %d\n",result);
+
+    result = sum(5,10);
+
+    printf("The SUM is = %d\n",result);
+}
 
