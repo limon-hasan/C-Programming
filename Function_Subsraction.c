0b7fe00b428faf2cfd01cd07a6311b4d59ca33e4
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-22 22:42:50 +0600

    Create Function_Subsraction.c

---
+++ Function_Subsraction.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+void sum(int a,int b, int c)
+{
+    printf("The SUM is = %d\n",a+b+c);
+}
+
+void sub(int a, int b)
+{
+    printf("Substraction = %d\n",a-b);
+}
+
+int main()
+{
+    sum(10,20,30);
+    sum(100,200,300);
+    sub(200,50);
+}
