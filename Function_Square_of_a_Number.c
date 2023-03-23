c3189e683a64fe56a5a1d89189c24d651e9c45eb
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-23 23:57:31 +0600

    Create Function_Square_of_a_Number.c

---
+++ Function_Square_of_a_Number.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    int num;
+    printf("Enter a number  = ");
+    scanf("%d",&num);
+
+    int value = square(num);
+}
+
+int square(int a){
+printf("The value of %d is %d",a,a*a);
+
+}
