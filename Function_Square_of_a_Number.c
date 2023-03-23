8545bd31419cf63ce56f487547a27e8882bed2fa
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-24 02:40:30 +0600

    Update Function_Square_of_a_Number.c

--- Function_Square_of_a_Number.c
+++ Function_Square_of_a_Number.c
@@ -2,13 +2,33 @@
 int main()
 {
     int num;
-    printf("Enter a number  = ");
+    printf("Enter a number = ");
     scanf("%d",&num);
 
     int value = square(num);
 }
 
-int square(int a){
-printf("The value of %d is %d",a,a*a);
-
+int square(int a)
+{
+    printf("The value of %d is %d",a,a*a);
 }
+
+
+Or------------------------------------------------------
+    
+#include<stdio.h>
+
+int square(int a)
+{
+printf("The value of %d is %d",a,a*a);
+}
+
+int main()
+{
+    int num;
+    printf("Enter a number = ");
+    scanf("%d",&num);
+
+    int value = square(num);
+}
+
