3a770bdd828cccb07e0f31e6aa6b660a6a7ed321
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-25 02:30:15 +0600

    Update Function_Array_passing.c

--- Function_Array_passing.c
+++ Function_Array_passing.c
@@ -113,6 +113,36 @@ Or----------------------------------------------------------
 void array(int num[], int size)
 {
     printf("The elements are : ");
+    for(int j=0; j<size; j++)
+    {
+        printf("%d ",num[j]);
+    }
+}
+
+int main()
+{
+   int a[5];
+    
+   printf("Enter the elements of the array : ");
+   for(int i=0; i<n; i++)
+   {
+       scanf("%d",&a[i]);
+   }
+
+    array(a,5);
+    
+    return 0;
+}    
+    
+
+Or----------------------------------------------------
+    
+    
+#include<stdio.h>
+
+void array(int num[], int size)
+{
+    printf("The elements are : ");
     for(int i=0; i<size; i++)
     {
         printf("%d ",num[i]);
