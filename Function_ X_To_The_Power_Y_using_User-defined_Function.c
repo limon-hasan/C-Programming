87ca402c3a65d1c7bcdf82c3088ea9ac8e27136e
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-24 06:15:03 +0600

    Create Function_ X_To_The_Power_Y_using_User-defined_Function.c

---
+++ Function_ X_To_The_Power_Y_using_User-defined_Function.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int calculatePower(int a, int b)
+{ int answer =1;
+    for(int i=1; i<=b; i++)
+    {
+        answer = answer*a;
+    }
+    return answer;
+}
+
+int main()
+{
+    int base,power;
+    printf("Enter base = ");
+    scanf("%d",&base);
+    printf("Enter power = ");
+    scanf("%d",&power);
+
+    int result = calculatePower(base,power);
+    printf("%d to the power %d = %d",base,power,result);
+}
