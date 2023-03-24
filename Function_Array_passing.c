cde19e4aa9067de05ddf39bba4b28242679e5c99
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-25 01:13:56 +0600

    Update Function_Array_passing.c

--- Function_Array_passing.c
+++ Function_Array_passing.c
@@ -16,3 +16,25 @@ int main()
 
 }
 
+
+Or--------------------------------------------------------------
+    
+#include<stdio.h>
+
+void array(int num[],int size)
+{
+    for(int i=0; i<5; i++)
+    {
+        printf("%d ",num[i]);
+    }
+}
+
+int main()
+{
+    int a[5]={10,20,30,40,50};
+
+    array(a,5);
+
+}
+
+    
