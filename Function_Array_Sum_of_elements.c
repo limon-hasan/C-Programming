df9a1439ce120f20a73973cde6b092a1cc96f36b
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-25 02:02:04 +0600

    Update Function_Array_Sum_of_elements.c

--- Function_Array_Sum_of_elements.c
+++ Function_Array_Sum_of_elements.c
@@ -28,3 +28,37 @@ int num_array(int num[], int size)
     }
     return sum;
 }
+
+
+Or-------------------------------------------------------------
+   
+   
+#include<stdio.h>
+
+int num_array(int num[], int size);
+
+int main()
+{
+   int a[5];
+
+   printf("Enter the elements of the array : ");
+   for(int i=0; i<5; i++)
+   {
+       scanf("%d",&a[i]);
+   }
+
+   int result = num_array(a,5);
+
+   return 0;
+}
+
+int num_array(int num[], int size)
+{
+    int sum=0;
+    for(int j=0; j<size; j++)
+    {
+        sum = sum + num[j];
+    }
+    printf("The Sum of the elemenys of the Array is %d",sum);
+}
+   
