f68a234a984283088004d7bd06ee828999a76837
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-03-26 16:38:52 +0600

    Update Function_Array_Variable_length_array.c

--- Function_Array_Variable_length_array.c
+++ Function_Array_Variable_length_array.c
@@ -37,3 +37,50 @@ int sum_array(int row_size,int col_size,int arr[row_size][col_size])
     }
     return sum;
 }
+
+
+Or------------------------------------------------------
+    
+    
+#include<stdio.h>
+
+int sum_array(int row_size,int col_size,int arr[row_size][col_size]);
+
+int main()
+{
+    int m,n;
+    printf("Enter row size = ");
+    scanf("%d",&m);
+    printf("Enter column size = ");
+    scanf("%d",&n);
+
+    int a[m][n];
+
+    for(int i=0; i<m; i++)
+    {
+        printf("Enter row %d : ",i+1);
+        for(int j=0; j<n; j++)
+        {
+            scanf("%d",&a[i][j]);
+        }
+    }
+
+    printf("The Sum of the elements of the array is %d",sum_array(m,n,a));
+
+}
+
+int sum_array(int row_size,int col_size,int arr[row_size][col_size])
+{
+
+
+    int sum=0;
+    for(int i=0; i<row_size; i++)
+    {
+        for(int j=0; j<col_size; j++)
+        {
+         sum = sum+arr[i][j];
+        }
+    }
+    return sum;
+}
+    
