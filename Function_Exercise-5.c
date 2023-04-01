c817649da718c65da6191d2a36879e4bd0457e3d
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-01 22:22:14 +0600

    Update Function_Exercise-5.c

--- Function_Exercise-5.c
+++ Function_Exercise-5.c
@@ -1,5 +1,45 @@
 #include<stdio.h>
 
+int OCCURRENCE(int ARRAY[], int a);
+
+int main()
+{
+    int A[5],num,n;
+
+    printf("Enter elements : ");
+    for(int i=0; i<5; i++)
+    {
+        scanf("%d",&A[i]);
+    }
+
+    printf("Enter the number = ");
+    scanf("%d",&num);
+
+    int result = OCCURRENCE(A,num);
+    printf("Enter the occurrences of %d is %d",num,result);
+
+    return 0;
+}
+
+int OCCURRENCE(int ARRAY[], int a)
+{
+    int count=0;
+    for(int i=0; i<5; i++)
+    {
+        if(a==ARRAY[i])
+            count++;
+    }
+
+    return count;
+
+}
+
+
+Or------------------------------------------------------------------------------
+
+
+#include<stdio.h>
+
 void OCCURRENCE(int a, int b, int ARRAY[]);
 
 int main()
