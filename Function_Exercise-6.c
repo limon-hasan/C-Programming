aba239438273c5de332b8f84bd9c9fc1ff672135
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-02 13:36:45 +0600

    Update Function_Exercise-6.c

--- Function_Exercise-6.c
+++ Function_Exercise-6.c
@@ -1,5 +1,60 @@
 #include<stdio.h>
 
+int del(int arr[], int size) ;
+
+int main()
+{
+    int A[50],i,n;    // n=5
+    printf("Enter the size of the array = ");
+    scanf("%d",&n);
+
+    printf("Enter the elements : ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&A[i]);
+    }
+    for(i=0; i<n; i++)
+    {
+        printf("\n%d",A[i]);
+    }
+
+    del(A,n);
+    return 0;
+}
+
+int del(int arr[], int size)
+{
+    int pos;
+    printf("\nFrom which element you want to delete : ");
+    scanf("%d",&pos);  // pos=3
+
+    if(pos<1 || pos>size)
+        printf("Invalid Position\n");
+    else
+        // Deleting the element
+        for(int i=pos-1; i<size-1; i++)
+        {
+            arr[i]=arr[i+1];
+        }
+
+    size--;
+    // After deleting the element print the array
+    printf("Modified array after deleting the element:\n");
+    for(int i=0; i<size; i++
+    )
+    {
+        printf("%d ",arr[i]);
+    }
+
+}
+
+
+
+Or----------------------------------------------------------------
+
+
+#include<stdio.h>
+
 int main()
 {
     int A[50],i,n,pos;    // n=5
