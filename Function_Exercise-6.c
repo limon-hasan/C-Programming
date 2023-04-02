6a7d6cc9e20f9b7058afc3900e3a6de3d0e03373
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-02 13:56:54 +0600

    Update Function_Exercise-6.c

--- Function_Exercise-6.c
+++ Function_Exercise-6.c
@@ -51,7 +51,62 @@ void del(int arr[], int size)
 
 
 Or----------------------------------------------------------------
+    
+// Using Variable Length Array
+    
+#include<stdio.h>
 
+void del(int arr[], int size);
+
+int main()
+{
+    int n,i;
+    printf("Enter the size of the array = ");
+    scanf("%d",&n);
+    int A[n];
+
+    printf("Enter the elements : ");
+    for(i=0; i<n; i++)
+    {
+        scanf("%d",&A[i]);
+    }
+    for(i=0; i<n; i++)
+    {
+        printf("\n%d",A[i]);
+    }
+
+    del(A,n);
+    return 0;
+}
+
+void del(int arr[], int size)
+{
+    int pos;
+    printf("\nFrom which element you want to delete : ");
+    scanf("%d",&pos);  // pos=3
+
+    if(pos<1 || pos>size)
+        printf("Invalid Position\n");
+    else
+        // Deleting the element
+        for(int i=pos-1; i<size-1; i++)
+        {
+            arr[i]=arr[i+1];
+        }
+
+    size--;
+    // After deleting the element print the array
+    printf("Modified array after deleting the element:\n");
+    for(int i=0; i<size; i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+}
+
+
+Or-----------------------------------------------------------------------
+    
 
 #include<stdio.h>
 
