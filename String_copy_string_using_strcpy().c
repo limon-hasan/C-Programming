4950f94647cfebdd47c9682ab8a607c540b62ceb
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-19 12:32:22 +0600

    Create String_copy_string_using_strcpy().c

---
+++ String_copy_string_using_strcpy().c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+    char source[] = "Maksudul Hasan Limon";
+    char target[30];
+
+    strcpy(target,source);
+
+    printf("Source string  = %s\n",source);
+    printf("Target string  = %s",target);
+
+
+
+}
