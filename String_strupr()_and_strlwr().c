f52745d21ff1c32f2507e25455c03a3df8d867a8
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-21 22:15:21 +0600

    Create String_strupr()_and_strlwr().c

---
+++ String_strupr()_and_strlwr().c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    char str[] = "Maksudul Hasan";
+    
+    strupr(str);
+
+    printf("str = %s\n",str);
+
+}
+
+Or---------------------------------------
+  
+#include<stdio.h>
+int main()
+{
+    char str[] = "Maksudul Hasan";
+
+    strlwr(str);
+
+    printf("str = %s\n",str);
+
+}
+  
