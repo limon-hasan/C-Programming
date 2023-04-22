8a384ad694aa61c205f63fd8477bee7946a08750
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-23 00:06:33 +0600

    Create Structure_Input_structure_element.c

---
+++ Structure_Input_structure_element.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+//global structure
+struct Person
+{
+    int age;
+    float salary;
+};
+
+int main()
+{
+    struct Person person1,person2;   // local variable
+
+    printf("Enter information for person 1 :\n");
+    printf("Enter Age :");
+    scanf("%d",&person1.age);
+    printf("Enter salary :");
+    scanf("%f",&person1.salary);
+
+
+    printf("Person 1 :\n");
+    printf("Age = %d\n",person1.age);
+    printf("Salary = %f\n",person1.salary);
+
+    printf("\nEnter information for person 2 :\n");
+    printf("Enter Age :");
+    scanf("%d",&person2.age);
+    printf("Enter salary :");
+    scanf("%f",&person2.salary);
+
+    printf("Person 2 :\n");
+    printf("Age = %d\n",person2.age);
+    printf("Salary = %f\n",person2.salary);
+
+    return 0;
+
+}
