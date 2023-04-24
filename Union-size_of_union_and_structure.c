2e338273aee7849b6887e736da46d37d318734ca
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-25 02:07:37 +0600

    Create Union-size_of_union_and_structure.c

---
+++ Union-size_of_union_and_structure.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+union test1
+{
+    int x,y;
+};
+
+union test2
+{
+    double ch;
+    int x;
+};
+
+union test3
+{
+    char name;
+    double d;
+};
+
+struct test4
+{
+ int x;
+ int y;
+ char ch;
+ float f;
+ double d;
+};
+int main()
+{
+    union test1 t1;
+    union test2 t2;
+    union test3 t3;
+    struct test4 t4;
+
+    printf("size of t1 = %d\n",sizeof(t1));
+    printf("size of t2 = %d\n",sizeof(t2));
+    printf("size of t3 = %d\n",sizeof(t3));
+    printf("size of t4 = %d\n",sizeof(t4));    //Ekhane structure er khetre shobgula member er alada alada kore sum korar poreo compiler extra kore 3 jog kore shathe abar 
+
+}
