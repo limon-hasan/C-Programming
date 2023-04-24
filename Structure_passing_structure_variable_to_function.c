c9c19f6a27880072051dc0fd2d5338cd7f231b38
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-24 13:38:28 +0600

    Create Structure_passing_structure_variable_to_function.c

---
+++ Structure_passing_structure_variable_to_function.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+struct Person
+{
+    char name[40];
+    int age;
+    float salary;
+};
+
+void display(struct Person p)
+{
+    printf("\nName : %s\n",p.name);
+    printf("Age : %d\n",p.age);
+    printf("salary : %f\n",p.salary);
+}
+
+int main()
+{
+    struct Person person1,person2;
+
+    strcpy(person1.name,"Maksudul Hasan Limon");   // structure er moddhe string oivabe declare kora jay na tai strcpy function use kore kora hoyeche
+    person1.age = 21;
+    person1.salary = 12390.34;
+    display(person1);
+
+    strcpy(person2.name,"Anisul Islam");
+    person2.age = 40;
+    person2.salary = 30450.45;
+    display(person2);
+
+    return 0;
+}
