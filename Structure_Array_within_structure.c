f78f41e44eeecaa0eb4fea909a6fcb00a7f760a4
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-24 12:23:22 +0600

    Update Structure_Array_within_structure.c

--- Structure_Array_within_structure.c
+++ Structure_Array_within_structure.c
@@ -15,8 +15,9 @@ int main()
         printf("Enter information for Person : %d\n",i+1);
         printf("Enter Name : ");
         fflush(stdin);                          /* Ekhane ei function ta newa hoise karon name (char data) input new jacchilo na int data input newar por tai.
-        fgets(person[i].name,20,stdin);            Ekhane shudhu abar scanf diye korle oi shomossha hobe na kintu tokhon ekta naam er er por ar space dewa jabe na. Ar evabe korle space dewa jabe    
+                                                   Ekhane shudhu abar scanf diye korle oi shomossha hobe na kintu tokhon ekta naam er er por ar space dewa jabe na. Ar evabe korle space dewa jabe    
                                                    Evabe---  scanf("%s",person[i].name);  */
+                                                  
         fgets(person[i].name,20,stdin);
         printf("Enter Age = ");
         scanf("%d",&person[i].age);
