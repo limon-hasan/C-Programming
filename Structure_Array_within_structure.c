fcd35d9940faf4189a192a10c68f5d29a3aef48e
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-24 11:40:59 +0600

    Update Structure_Array_within_structure.c

--- Structure_Array_within_structure.c
+++ Structure_Array_within_structure.c
@@ -14,9 +14,9 @@ int main()
     {
         printf("Enter information for Person : %d\n",i+1);
         printf("Enter Name : ");
-        fflush(stdin);              // Ekhane ei function ta newa hoise karon name (char data) input new jacchilo na int data input newar por tai.
-                                    // Ekhane shudhu abar scanf diye korle oi shomossha hobe na kintu tokhon ekta naam er er por ar space dewa jabe na. Ar evabe korle space dewa jabe    
-                                    // Evabe---  scanf("%s",person[i].name);
+        fflush(stdin);              /* Ekhane ei function ta newa hoise karon name (char data) input new jacchilo na int data input newar por tai.
+                                       Ekhane shudhu abar scanf diye korle oi shomossha hobe na kintu tokhon ekta naam er er por ar space dewa jabe na. Ar evabe korle space dewa jabe    
+                                       Evabe---  scanf("%s",person[i].name);  */
         
         fgets(person[i].name,20,stdin);
         printf("Enter Age = ");
