accfcc3c6fbe55e3288b2b653ac3ca80ab01cb3a
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-25 02:09:01 +0600

    Update Union-size_of_union_and_structure.c

--- Union-size_of_union_and_structure.c
+++ Union-size_of_union_and_structure.c
@@ -35,6 +35,6 @@ int main()
     printf("size of t1 = %d\n",sizeof(t1));
     printf("size of t2 = %d\n",sizeof(t2));
     printf("size of t3 = %d\n",sizeof(t3));
-    printf("size of t4 = %d\n",sizeof(t4));    //Ekhane structure er khetre shobgula member er alada alada kore sum korar poreo compiler extra kore 3 jog kore shathe abar 
+    printf("size of t4 = %d\n",sizeof(t4));    //Ekhane structure er khetre shobgula member er alada alada kore sum korar poreo compiler extra 3 jog kore shathe abar 
 
 }
