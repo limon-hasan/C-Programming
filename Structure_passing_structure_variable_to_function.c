ea357c498e8579592e04fb1b6168af74a51943d0
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-25 20:03:56 +0600

    Update Structure_passing_structure_variable_to_function.c

--- Structure_passing_structure_variable_to_function.c
+++ Structure_passing_structure_variable_to_function.c
@@ -18,7 +18,7 @@ int main()
 {
     struct Person person1,person2;
 
-    strcpy(person1.name,"Maksudul Hasan Limon");   // structure er moddhe string oivabe declare kora jay na tai strcpy function use kore kora hoyeche
+    strcpy(person1.name,"Maksudul Hasan Limon");   // structure er moddhe string eivabe mane (person1.name = {"Maksudul"}) declare kora jay na tai strcpy function use kore kora hoyeche
     person1.age = 21;
     person1.salary = 12390.34;
     display(person1);
