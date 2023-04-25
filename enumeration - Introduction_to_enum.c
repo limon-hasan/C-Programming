4cfb20571c47ccfe86d506b322cef72467f40dd6
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-25 14:03:53 +0600

    Update enumeration - Introduction_to_enum.c

--- enumeration - Introduction_to_enum.c
+++ enumeration - Introduction_to_enum.c
@@ -12,3 +12,21 @@ int main()
     printf("Todays difference = %d",today1-today2);
 
 }
+
+
+or-------------------
+    
+#include<stdio.h>
+
+enum days_of_week {Sun=1,Mon=4,Tue,Wed,Thu,Fri,Sat};
+
+int main()
+{
+    enum days_of_week today1,today2;
+
+    today1 = Sat;
+    today2 = Sun;
+
+    printf("Todays difference = %d",Sat-Thu);
+
+}
