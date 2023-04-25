a9c6801ea1fa052ea0bf1096c38a83e3f2864d3e
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-25 20:33:34 +0600

    Update typedef.c

--- typedef.c
+++ typedef.c
@@ -15,5 +15,26 @@ int main()
     ch2 = 'B';
     printf("ch2 = %c\n",ch2);
 
+}
+
+Or-----------------------
+
+#include<stdio.h>
+
+struct book
+{
+    char name[20];
+    int price;
+};
+
+int main()
+{
+
+    typedef struct book BOOK;
+    BOOK b = {"Maksudul",78};
+
+    printf("Name = %s\n",b.name);
+    printf("Price = %d",b.price);
 
 }
+    
