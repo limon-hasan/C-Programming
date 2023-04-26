d12ba234bf768cd76b184b5ceb021f4dbdd7fe5c
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-26 20:42:35 +0600

    Create File-Introduction_to_File.c

---
+++ File-Introduction_to_File.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    FILE *file;
+
+    file = fopen("test.txt","w");
+
+    if(file==NULL)
+        printf("File doesn't exist");
+    else
+    {
+        printf("File is opened");
+        fclose(file);
+    }
+    
+}
