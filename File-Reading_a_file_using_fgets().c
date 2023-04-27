dbbc5f0bc51035382af505aeee1bfca4903fd8fa
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-27 20:31:16 +0600

    Update File-Reading_a_file_using_fgets().c

--- File-Reading_a_file_using_fgets().c
+++ File-Reading_a_file_using_fgets().c
@@ -16,7 +16,7 @@ int main()
         {
              fgets(ch,32,fp);    //fgets function e 3ta parameter thake. fgets(variable,size,filePointer) --> ei variable tate file data read kore rakhbe
              printf("%s",ch);    // ekhane size bole dewar lagbe, duita line e jodi same character na thake tahole ekta line e highest jeita shei size ta nibo ei khetere
-        }
+        }                        // Amar file er line e 32 ta character chilo highest ejonno ami 32 diyechi
 
         fclose(fp);
     }
