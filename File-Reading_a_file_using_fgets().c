a391fc87b1233412edf14517f55b59d80c12b215
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-27 20:29:50 +0600

    Create File-Reading_a_file_using_fgets().c

---
+++ File-Reading_a_file_using_fgets().c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    FILE *fp;
+    char ch[40];
+
+    fp = fopen("file.txt","r");
+
+    if(fp==NULL)
+        printf("File doesn't exist");
+    else
+    {
+        printf("FIle is opened\n");
+
+        while(!feof(fp))
+        {
+             fgets(ch,32,fp);    //fgets function e 3ta parameter thake. fgets(variable,size,filePointer) --> ei variable tate file data read kore rakhbe
+             printf("%s",ch);    // ekhane size bole dewar lagbe, duita line e jodi same character na thake tahole ekta line e highest jeita shei size ta nibo ei khetere
+        }
+
+        fclose(fp);
+    }
+
+}
