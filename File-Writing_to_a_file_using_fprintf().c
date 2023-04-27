db7d1dc0eb4a0706d1c9d2711a1b478273a7b760
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-27 13:12:48 +0600

    Update File-Writing_to_a_file_using_fprintf().c

--- File-Writing_to_a_file_using_fprintf().c
+++ File-Writing_to_a_file_using_fprintf().c
@@ -24,3 +24,34 @@ int main()
     }
 
 }
+
+or-------------------------------------   ( "a" mode )
+    
+#include<stdio.h>
+int main()
+{
+    FILE *file;
+    char name[30];
+    int age;
+
+    file = fopen("file.txt","a");
+
+    if(file==NULL)
+        printf("File doesn't exist");
+    else
+    {
+        printf("File is opened\n");
+        printf("Enter your name : ");
+        gets(name);
+
+        printf("Enter age : ");
+        scanf("%d",&age);
+
+        fprintf(file,"Name = %s, Age = %d\n",name,age);     //fprintf(filepointer,"format specifier",variable)
+        printf("File is written successfully");
+        fclose(file);
+    }
+
+}
+    
+    
