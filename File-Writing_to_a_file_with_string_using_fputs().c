6b299fc2dcdf6fc0bb5e8cb7753035649235ad24
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-27 12:36:47 +0600

    Update File-Writing_to_a_file_with_string_using_fputs().c

--- File-Writing_to_a_file_with_string_using_fputs().c
+++ File-Writing_to_a_file_with_string_using_fputs().c
@@ -20,3 +20,30 @@ int main()
     }
 
 }
+
+Or---------------------------------------
+    
+#include<stdio.h>
+int main()
+{
+    FILE *file;
+    char name[30];
+
+    file = fopen("file.txt","a");    // file mode "a" dile file e ager lekhar shathe notun kisu add hobe ar shudhu "w" dile ager lekha muchee giye notun ksu lekha hobe
+
+    if(file==NULL)
+        printf("File doesn't exist");
+    else
+    {
+        printf("File is opened\n");
+        printf("Enter your University name : ");
+        gets(name);
+
+        //fputs(name,file);
+        fprintf(file,"\n%s",name);
+        printf("File is written successfully");
+        fclose(file);
+    }
+
+}
+    
