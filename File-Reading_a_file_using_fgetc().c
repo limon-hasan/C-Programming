4029ea71329452e9a7daa423e17ebb8a39946c9c
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-27 19:55:06 +0600

    Create File-Reading_a_file_using_fgetc().c

---
+++ File-Reading_a_file_using_fgetc().c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+    FILE *fp;
+    char ch;
+
+    fp = fopen("file.txt","r");
+
+    if(fp==NULL)
+        printf("File doesn't exist");
+    else
+    {
+        printf("FIle is opened\n");
+        
+        while(!feof(fp))  // File shuru theke shesh porjonto read korbo amra ejonno file end of function use korechi while loop er vitore 
+        {
+            ch = fgetc(fp);  //fgetc() ei function er maddhome amra ekta ekta kore character nite parbo mane fgetc function ta ekta ekta kore character amader dite thakbe ejonno ekta char typer variable declare kore nibo character gula newar jonno
+            printf("%c",ch);
+        }
+        
+        fclose(fp);
+    }
+
+}
