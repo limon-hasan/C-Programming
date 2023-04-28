5a03c4a0795c041d91bc6a9b501905b6b37534c1
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-28 12:27:06 +0600

    Update File-Reading_a_file_using_fscanf().c

--- File-Reading_a_file_using_fscanf().c
+++ File-Reading_a_file_using_fscanf().c
@@ -21,3 +21,31 @@ int main()
     }
 
 }
+
+
+Or----------------------------------------------------
+    
+#include<stdio.h>
+int main()
+{
+    FILE *fp;
+    char ch1[50];
+    char ch2[20];
+    int roll;
+
+    fp = fopen("file.txt","r");
+
+    if(fp==NULL)
+        printf("File doesn't exist");
+    else
+    {
+        printf("FIle is opened\n");
+
+        fscanf(fp,"%s %s %d",&ch1,&ch2,&roll);    //fscanf() function e 3 ta parameter thake. fscanf(filePointer,format specifier,variable)
+        printf("%s %s %d",ch1,ch2,roll);         /* Output e jodi Limon Hasan thake tahole eita ekta string hishebe count korle hobe na karo %s tokhon Limon er pore Hasan print korte pare na 
+                                            ejonno "Limon Hasan" duita string dhorte hobe ebong Limon er pore jehetu space ache tai ekta %s diye pore space diye arekta %s dite hobe */
+        fclose(fp);
+    }
+
+}
+    
