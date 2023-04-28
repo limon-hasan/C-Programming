28188af32f9c93afbd19fbcf530bd1c8c1b2a8ee
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-28 11:58:55 +0600

    Update File-Reading_a_file_using_fscanf().c

--- File-Reading_a_file_using_fscanf().c
+++ File-Reading_a_file_using_fscanf().c
@@ -15,7 +15,7 @@ int main()
         printf("FIle is opened\n");
 
         fscanf(fp,"%s %s",&ch1,&ch2);    //fscanf() function e 3 ta parameter thake. fscanf(filePointer,format specifier,variable)
-        printf("%s %s",ch1,ch2);         /* Output e jodi Limon Hasan thake tahole eita ekta string hishebe count korle hobe na karo %s tokhon Limon er pore Hasan print korte pare na ejonno "Limon Hasan" duita string dhorte hobe ebong Limon er pore jehetu space ache tai ekta %s diye pore space diye arekta %s dite hobe
+        printf("%s %s",ch1,ch2);         /* Output e jodi Limon Hasan thake tahole eita ekta string hishebe count korle hobe na karo %s tokhon Limon er pore Hasan print korte pare na
                                             ejonno "Limon Hasan" duita string dhorte hobe ebong Limon er pore jehetu space ache tai ekta %s diye pore space diye arekta %s dite hobe */
         fclose(fp);
     }
