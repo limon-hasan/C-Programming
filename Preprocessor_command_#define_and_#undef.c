9722671daaccb80f3d698d6a31813e20f8d16672
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-29 21:11:23 +0600

    Update Preprocessor_command_#define_and_#undef.c

--- Preprocessor_command_#define_and_#undef.c
+++ Preprocessor_command_#define_and_#undef.c
@@ -21,3 +21,19 @@ int main()
 }
 
 
+Or----------------------------------------
+    
+#include<stdio.h>
+
+#define MAX(a,b) if(a>b) \
+                   printf("%d is maximum",a); \
+                   else \
+                    printf("%d is maximum",b);
+
+int main()
+{
+    MAX(3,4);
+
+}
+
+    
