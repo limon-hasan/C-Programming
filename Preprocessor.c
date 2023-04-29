731ad6d050ad72ebc1966af8eb2f523c247d2f31
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-29 12:54:58 +0600

    Create Preprocessor.c

---
+++ Preprocessor.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+//#define name "Anisul Islam"
+//char name[] = "Anisul Islam";
+//#define ch 'A'
+
+#define s printf("East West University");
+
+int main()
+{
+    //printf("%s\n",name);
+    //printf("%c\n",ch);
+
+    s
+
+    getch();
+}
+
+
+
+
+
+
