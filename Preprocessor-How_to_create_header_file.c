6c5680678515bad4c13d8cb3a095456a03a0c379
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-29 13:47:29 +0600

    Update Preprocessor-How_to_create_header_file.c

--- Preprocessor-How_to_create_header_file.c
+++ Preprocessor-How_to_create_header_file.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#include "info.h"
+#include "info.h"      // header file inc;ude korte hole #include likhe tarpor "" er vitor likhte hoy
 
 int main()
 {
@@ -14,6 +14,25 @@ int main()
 info.h header file e nicher data gula store kore niyechi
 #define name "Maksudul Hasan"
 #define age 27
+
+
+Or----------------------------------------------
+    
+    
+#include <stdio.h>
+#include "header.h"    
+int main()
+{
+    printf("Age = %d\n");
+    printf("PI = %f",PI);
+    lu
+    return 0;
+}
+    
+
+#define age 45
+#define PI 3.1416
+#define lu printf("\nEast West University");
 
 
 
