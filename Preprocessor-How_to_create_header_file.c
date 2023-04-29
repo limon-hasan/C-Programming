68df1658829b80a2049b018c067a4d3c55f3fa91
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-04-29 13:41:16 +0600

    Create Preprocessor-How_to_create_header_file.c

---
+++ Preprocessor-How_to_create_header_file.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include "info.h"
+
+int main()
+{
+
+    printf("Name = %s\n",name);
+    printf("Age = %d",age);
+
+    getch();
+}
+
+
+info.h header file e nicher data gula store kore niyechi
+#define name "Maksudul Hasan"
+#define age 27
+
+
+
+
