187105c677a76ffa6fcd05e0a89344bb3df17af3
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-05-06 22:13:36 +0600

    Create realloc().c

---
+++ realloc().c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main()
+{
+    int n1,n2,i,*ptr;
+    printf("Enter the size = ");
+    scanf("%d",&n1);
+
+    *ptr = (int*) malloc(n1 *sizeof(int));
+    printf("Addresses of previously allocated memory:\n");
+    for(int i=0; i<n1; i++)
+    {
+        printf("%p\n",ptr+i);
+    }
+
+    printf("Enter the new size = ");
+    scanf("%d",&n2);
+    ptr = realloc(ptr,n2* sizeof(int));
+
+    printf("Addresses of newly allocated memory:\n");
+    for(i=0; i<n2; i++)
+        printf("%p\n",ptr+i);
+
+    free(ptr);
+
+}
+
+
