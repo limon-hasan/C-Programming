1decf929a3bbd6584898a45e0ba75a53a9666d4b
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-05-12 16:06:17 +0600

    Update String-ACCESSING_STRING_VIA_POINTER.c

--- String-ACCESSING_STRING_VIA_POINTER.c
+++ String-ACCESSING_STRING_VIA_POINTER.c
@@ -13,3 +13,23 @@ int main()
     
     return 0;
 }
+
+Or------------------------------------
+    
+#include <stdio.h>
+
+int main()
+{
+    char str[] = "Maksudul Hasan Limon";
+    char *ptr;
+    ptr = str;
+
+    while(*ptr!='\0')
+    {
+        printf("%c",*ptr);
+        *ptr++;
+    }
+
+    return 0;
+}
+    
