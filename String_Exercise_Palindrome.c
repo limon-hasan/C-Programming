5ce767e4915b751525dab594c5ddc67c6616626f
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-05-14 11:36:53 +0600

    Update String_Exercise_Palindrome.c

--- String_Exercise_Palindrome.c
+++ String_Exercise_Palindrome.c
@@ -51,3 +51,51 @@ bool is_palindrome(char *s1)
     return true;
 }
 
+
+Or------------------------------------------------------
+    
+    
+#include<stdio.h>
+#include<stdbool.h>
+
+bool is_palindrome(char *s1);
+
+int main()
+{
+    char str[40];
+    printf("Enter a message: ");
+    fgets(str,40,stdin);
+
+    char result = is_palindrome(str);
+    if(result == true)
+        printf("String is palindrome\n");
+    else
+        printf("String is not palindrome\n");
+
+    return 0;
+}
+
+bool is_palindrome(char *s1)
+{
+    int left = 0;
+    int right = strlen(s1) - 1;
+
+    while(left<right)
+    {
+        while(!isalpha(s1[left]))
+        {
+            left++;
+        }
+
+        while(!isalpha(s1[right]))
+        {
+            right--;
+        }
+        if(tolower(s1[left])!= tolower(s1[right]))
+            return false;
+
+        left++;
+        right--;
+    }
+    return true;
+}   
