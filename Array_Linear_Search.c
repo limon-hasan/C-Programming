f4d6a02a153f202c3f749176d10e04c27160f65c
Maksudul hasan Limon <120485933+limon-hasan@users.noreply.github.com>
2023-11-16 01:41:53 +0600

    Update Array_Linear_Search.c

--- Array_Linear_Search.c
+++ Array_Linear_Search.c
@@ -29,6 +29,45 @@ int main()
 }
 
 
+---------------------------------------------------
+
+// Using function of above of this code :
+
+#include <stdio.h>
+int linearSearch(int arr[], int size, int key, int *position) {
+    for (int i = 0; i < size; i++) {
+        if (key == arr[i]) {
+            *position = i;
+            return 1; // Found
+        }
+    }
+    return 0; // Not found
+}
+
+int main() {
+    while (1) {
+        int a[30], n, value, pos;
+
+        printf("Enter how many numbers = ");
+        scanf("%d", &n);
+        printf("Enter the value = ");
+        scanf("%d", &value);
+
+        for (int i = 0; i < n; i++) {
+            scanf("%d", &a[i]);
+        }
+
+        if (linearSearch(a, n, value, &pos)) {
+            printf("Position is = %d\n", pos);
+        } else {
+            printf("Not Found\n");
+        }
+    }
+
+    return 0;
+}
+        
+
 Or,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
 
 #include<stdio.h>
